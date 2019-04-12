5cbf7a3c0c63a4285f9b1800ba30ee00df05fa79
Sanjay Sai <shachsan@hotmail.com>
2019-04-12 14:07:17 -0400

    update user model to accept user details

--- models/user.js
+++ models/user.js
@@ -2,7 +2,25 @@ const mongoose = require('mongoose');
 
 const User = mongoose.Schema({
     username:{type: String, required:true},
-    password:{type: String, required:true}
+    password:{type: String, required:true},
+    first_name:{type: String, required:true},
+    middle_initial:String,
+    last_name:{type: String, required:true},
+    dob:Date,
+    street_address:String,
+    apt:String,
+    city:String,
+    state:String,
+    zipcode:Number,
+    billing_street_address: String,
+    billing_apt:String,
+    billing_city:String,
+    billing_state:String,
+    billing_zipcode:Number,
+    fullName:String,
+    cardNo:Number,
+    card_expiry:String,
+    secret:Number
 })
 
 module.exports = mongoose.model('User', User);
