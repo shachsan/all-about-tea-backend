3d9482f7012c7187c88f1e8782507bf5883016a1
Sanjay Sai <shachsan@hotmail.com>
2019-04-12 08:54:57 -0400

    added gitignore file

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const User = require('../models/user');
 const Bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -25,30 +26,41 @@ router.post('/login', (req, res, next)=>{
     // check if the user exist with the email address
     // if user exist, verify the password provided matches with the password in the database
     // if password is authenticated, create a token for this user and send back to the user as response
-        console.log('=====req.body.email=======>',req.body);
+        // console.log('=====req.body.email=======>',req.body);
         User.findOne({username:req.body.username})
             .then(user=>{
-                console.log('=======user========>:', user);
+                // console.log('=======user========>:', user);
                 if(!user){
-                    console.log('=========Wrong email =============');
+                    // console.log('=========Wrong email =============');
                     return res.status(401).json({
                         message:"User not found"
                     });
                 }
                 
-                console.log('===========Email found, checking for password match=================');
+                // console.log('===========Email found, checking for password match=================');
                 //below code is executed if user is found
                 // verify the password
-                return Bcrypt.compare(req.body.password, user.password)
-            })
-            .then(result=>{
-                // console.log('result', result);
-                if(!result){
-                    console.log('=======Password did not match ==========');
-                    return res.status(401).json({message:"Auth failed"});
-                }
-                console.log('==========email and password both matched=========');
-                res.status(200).json({message: "User successfully logged in"});
+                Bcrypt.compare(req.body.password, user.password)
+                    .then(result=>{
+                    // console.log('result', result);
+                    if(!result){
+                        // console.log('=======Password did not match ==========');
+                        return res.status(401).json({message:"Auth failed"});
+                    }
+                    // console.log('==========email and password both matched=========');
+                    const token = jwt.sign(
+                        {username:user.username, userId:user._id}, 
+                        'shhhh do not tell any one', 
+                        {expiresIn:'1h'})
+                    if(!token){
+                        console.log('=====token creattion error=====>>>',err);
+                    }
+                        
+                    console.log('=======token========', token);
+                    res.status(200).json({message: "User successfully logged in", token:token});
+                }).catch(err => {
+                    res.status(401).json({error_message:err})
+                })
             })
             .catch(error=>{
                 console.log('error====>', error);
